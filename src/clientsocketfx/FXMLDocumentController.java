f3b4bf24f51438f676ca235f179847235e295f29
lvwagner <lvwagner@gmail.com>
2015-06-10 10:02:06 -0400

    Primeiro commit

---
+++ src/clientsocketfx/FXMLDocumentController.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientsocketfx;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author wagner
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private Button btnEnviar;
+
+    @FXML
+    private TextField tf_message;
+
+    @FXML
+    private TextArea txArea;
+
+    Socket socket;
+    InputStream inStream;
+    InputStreamReader inReader;
+    BufferedReader bufReader;
+    PrintStream printStream;
+    PrintWriter pWriter;
+    Thread trd;
+
+    @FXML
+    void enviarMenssagem(ActionEvent event) {
+        printStream.println(tf_message.getText());
+        printStream.flush();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        try {
+            System.out.println("client online");
+            socket = new Socket("localhost", 2222);
+            printStream = new PrintStream(socket.getOutputStream());
+            inStream = socket.getInputStream();
+            inReader = new InputStreamReader(inStream);
+            bufReader = new BufferedReader(inReader);
+
+            trd = new Thread(() -> {
+                while (true) {
+                    try {
+                        String msg = bufReader.readLine();
+                        txArea.appendText("-> " + msg + "\n");
+                    } catch (IOException ex) {
+                        Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            });
+            trd.start();
+        } catch (Exception e) {
+        }
+    }
+}
